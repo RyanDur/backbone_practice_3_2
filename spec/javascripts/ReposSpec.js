9f649aefaefc15f20db8d38abd68f6e1e1b943ca
RyanDur <ryan.p.durling@gmail.com>
2013-07-31 22:24:26 -0700

    test with jasmine jasmine-jquery and sinon

--- spec/javascripts/ReposSpec.js
+++ spec/javascripts/ReposSpec.js
@@ -1,17 +1,36 @@
 describe('repos', function() {
-  var repos, data, sortedData;
+  var server, repos;
 
   beforeEach(function() {
-    sortedData = ['Project_Euler', 'jquery-practice', 'ActiveRecordPractice', 'rackPractice', 'JavascriptPractice', 
-                      'Angular_Practice', 'my_emacs', 'JC_Angular', 'Patu', 'LearninWithMyCoach'];
+    var response = getJSONFixture('git.json');
+    server = sinon.fakeServer.create();
 
-    data = getJSONFixture('git.json');
-    repos = new Bbprac32.Collections.Repos(data);
+    server.respondWith(
+      "GET",
+      "/api/repos",
+      [
+        200,
+        {"Content-Type": "application/json"},
+        JSON.stringify(response)
+      ]
+    );
+
+    repos = new Bbprac32.Collections.Repos();
+    repos.fetch();
+    server.respond();
+  });
+
+  afterEach(function() {
+    server.restore();
   });
 
   it('should sort the data by updated_at', function() {
+    var sortedData = ['Project_Euler' ,'jquery-practice', 'ActiveRecordPractice', 'rackPractice', 'JavascriptPractice', 
+                      'Angular_Practice', 'my_emacs', 'JC_Angular', 'Patu', 'LearninWithMyCoach'];
+
+    expect(repos.length).toEqual(10);
     repos.each(function(repo, index) {
-      expect(repo.get('name')).toEqual(sortedData[index]);
+      expect(repo.get('name')).toEqual(sortedData[index])
     });
   });
 });
